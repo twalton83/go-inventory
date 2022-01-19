1ddcd0a61b9efd59eb88f165bd86385a6d7bd130
Tatiana <hello@tatianacodes.com>
2022-01-18 19:54:20 -0800

    Set up helper for info

--- main.go
+++ main.go
@@ -14,20 +14,31 @@ type Category struct {
 	Items []Item
 }
 
+var categories []Category
+var defaultCategory = Category{Name: "General", Items: make([]Item, 0)}
+
+
+
 func (c Category) AddItem(i Item){
 
 }
 
-
-
-
 func main() {
+	setUpDefaultInfo()
 	fmt.Println("==== INVENTORY APP ====")
-	defaultItem := Item{"Chewing Gum", 1.99}
-	defaultCategory := Category{Name: "General", Items: make([]Item, 0)}
-	defaultCategory.Items = append(defaultCategory.Items, defaultItem)
-	var categories []Category
-	categories = append(categories, defaultCategory)
-
 	fmt.Printf("%+v \n", categories)
 }
+
+func setUpDefaultInfo () {
+	item_1, item_2, item_3 := Item{"Chewing Gum", 1.99}, Item{"Soda", .99}, Item{"Chips", 2.89}
+	defaultCategory.Items = append(defaultCategory.Items, item_1, item_2, item_3)
+	categories = append(categories, defaultCategory)
+}
+// Create user navigation
+	// Categories
+	// Items
+	// Add Category
+	// Delete Category
+	// Add Item To Category
+	// Remove Item From Category
+	// Back
