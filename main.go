9849eb68afa52db9038b4cf646baa050da0b6da3
Tatiana <hello@tatianacodes.com>
2022-01-20 21:03:44 -0800

    Add basic navigation

--- main.go
+++ main.go
@@ -24,15 +24,57 @@ func (c Category) AddItem(i Item){
 }
 
 func main() {
+	var input int
 	setUpDefaultInfo()
 	fmt.Println("==== INVENTORY APP ====")
-	fmt.Printf("%+v \n", categories)
+	printMainNavigation()
+	fmt.Scanf("%d", &input)
 }
 
 func setUpDefaultInfo () {
 	item_1, item_2, item_3 := Item{"Chewing Gum", 1.99}, Item{"Soda", .99}, Item{"Chips", 2.89}
 	defaultCategory.Items = append(defaultCategory.Items, item_1, item_2, item_3)
 	categories = append(categories, defaultCategory)
+}
+
+func printMainNavigation() {
+	var input int
+	fmt.Println("==== Navigation ====")
+	fmt.Println("1) Add a category \n 2) View categories \n 3) Add an Item \n 4) View an Item")
+	n, _ := fmt.Scanf("%d", &input)
+	if n == 0{
+		return 
+	}
+	routeMainNavigation(input)
+	
+}
+
+func routeMainNavigation(selection int) {
+	switch selection {
+	case 1:
+		
+		addNewCategory()
+	case 2:
+		fmt.Printf("%+v \n", categories)
+	case 3:
+		addNewItem()
+	case 4: 
+		viewItem()
+	default:
+		fmt.Printf("Invalid selection")
+	}
+}
+
+func addNewCategory(){
+	fmt.Printf("Name:")
+}
+
+func addNewItem(){
+	fmt.Printf("Name:")
+}
+
+func viewItem(){
+	fmt.Printf("Item Number:")
 }
 // Create user navigation
 	// Categories
